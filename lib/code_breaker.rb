10d74bfb9f5047946bcbf7c48670f89af0e9814f
sinec1 <187554295+sinec1@users.noreply.github.com>
2026-09-17 10:44:55 -0700

    Implement for user guessing and display correctness of guess

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -8,23 +8,28 @@ class Game
   def initialize(player_role)
     @player_role = player_role
     @code = Array.new(CODESIZE, nil)
-    @code_accuracy = Array.new(CODESIZE, nil)
+    @code_accuracy = Array.new(CODESIZE, NONE_CORRECT)
     @color_array = DEFAULT_COLORS.dup
     @colors = @color_array.size
   end
 
   def computer_create_code
     @code.map! {|entry| @color_array[rand(0...@colors)]}
+    puts @code
   end
 
   def guess
-    puts "Please input your #{@colors} color guess! (eg 'red, green, blue' for a 3 color guess in those positions)"
+    puts "Current Game Rules: #{@colors} colors, #{@code.size} color combination to guess."
+    puts "Please input your guess (eg 'red, green, blue' for a guess in those positions)"
     @current_guess = gets.downcase.gsub(/\s+/, "").split(',')
     unless validate_guess
       puts 'Please input a valid guess! (you may have guessed with the wrong number of colors)'
       @current_guess = gets.downcase.gsub(/\s+/, "").split(',')
     end
     check
+    puts @code_accuracy
+    displayCodeAccuracy
+    resetCodeAccuracy
   end
 
   def validate_guess
@@ -35,6 +40,38 @@ class Game
   end
 
   def check
-    
+    correct_indices = Array.new
+    CODESIZE.times do |idx|
+      if @current_guess[idx] == @code[idx]
+        @code_accuracy[idx] = ALL_CORRECT 
+        correct_indices.push(idx)
+      end
+    end
+
+    CODESIZE.times do |i|
+       @code.each_with_index do |code_entry, idx|
+        if code_entry == @current_guess[i] && !correct_indices.include?(idx)
+          @code_accuracy[i] = COLOR_CORRECT
+          correct_indices.push(idx)
+        end
+      end
+    end
   end
+
+  def resetCodeAccuracy
+    @code_accuracy.map! {|entry| entry = NONE_CORRECT}
+  end
+
+  def displayCodeAccuracy
+    @code_accuracy.each_with_index do |entry,idx|
+      print "Entry #{idx} of guess: "
+      puts "Correct color and position" if entry == ALL_CORRECT
+      puts "Correct color, wrong positon" if entry == COLOR_CORRECT
+      puts "Wrong color and position" if entry == NONE_CORRECT
+    end
+  end
+
+  test = Game.new('guesser')
+  test.computer_create_code
+  test.guess
 end
